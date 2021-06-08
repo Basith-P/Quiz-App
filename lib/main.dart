a63327cefb10dbb7b054b7c73b186851c15fc752
Basith P <basithpofficial@gmail.com>
2021-06-08 18:19:26 +0530

    Initial commit w/ Basic structure

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // ignore: todo
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favourite color?',
+      'What\s your favourite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Personality Quiz'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[_questionIndex],
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
