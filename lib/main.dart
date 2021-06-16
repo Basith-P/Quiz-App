e40c8c3326d4c6b4afeb531580c26731b70d6dbe
Basith P <basithpofficial@gmail.com>
2021-06-16 13:06:11 +0530

    Make question widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import './question.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
+            Question(
               questions[_questionIndex],
             ),
             ElevatedButton(
