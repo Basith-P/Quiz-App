73cc3d3bcbcb8fe44eb59fe30bca6c42e91d0593
Basith P <basithpofficial@gmail.com>
2021-06-24 13:14:14 +0530

    map lists to widgets

--- lib/answers.dart
+++ lib/answers.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function() selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +13,9 @@ class Answer extends StatelessWidget {
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           primary: Colors.amber,
+          onPrimary: Colors.black,
         ),
-        child: Text('Answer 1'),
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
